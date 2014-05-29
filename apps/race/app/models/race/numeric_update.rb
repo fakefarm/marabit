4486df922511d367a64a3725feccbd34a4b3f982
Dave Woodall <dmwoodall@gmail.com>
2014-05-28 18:42:27 -0600

    Add race_total scope to numeric updates

--- apps/race/app/models/race/numeric_update.rb
+++ apps/race/app/models/race/numeric_update.rb
@@ -1,6 +1,10 @@
 module Race
   class NumericUpdate < ActiveRecord::Base
+
+    scope :race_total, ->(race) { where("rush_id = ?", race) }
+
     belongs_to :rush
+
     validates_presence_of :quantity
   end
 end
