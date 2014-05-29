c2ffa56df9365882bf2aefb382e753e3ed9d594e
Dave Woodall <dmwoodall@gmail.com>
2014-05-28 19:04:38 -0600

    Add personal_score helper

--- apps/race/app/helpers/race/rushes_helper.rb
+++ apps/race/app/helpers/race/rushes_helper.rb
@@ -8,6 +8,11 @@ module Race
       end
     end
 
+    def personal_score(user, rush)
+      total ||= user.map { |n| if n.rush_id == rush.id; n.quantity end }.compact.reduce(:+)
+      pluralize(total, 'mile')
+    end
+
     def show_time_status(update)
       if update == nil
         distance_of_time_in_words(Time.now, update.last.created_at)
